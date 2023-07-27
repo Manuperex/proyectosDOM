4e0eed40b14dc830e3c1a591bd96330d6a6920bc
Manuperex <manuperex24@gmail.com>
2023-07-27 00:15:09 -0500

    first commit

---
+++ toppings-de-pizza/app.js
@@ -0,0 +1,17 @@
+// const titulo = document.getElementById('titulo');
+// console.log(titulo.innerText);
+
+// const toppings = document.getElementsByClassName('toppings');
+// console.log(toppings[0].id)
+
+// const misToppings = document.getElementsByTagName('li')
+// console.log(misToppings)
+
+// const toppingsMarron = document.getElementsByClassName('fondo-marron');
+// console.log(toppingsMarron);
+
+// const primerToppingsNoMarron = document.querySelector('ul li:not(.fondo-marron');
+// console.log(primerToppingsNoMarron)
+
+// const toppingsNaranja = document.querySelectorAll('.toppings.fondo-naranja');
+// console.log(toppingsNaranja)
