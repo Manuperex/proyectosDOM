e7d414aa8d8260d3ac68450876220858d511cdb9
Manuperex <manuperex24@gmail.com>
2023-07-28 15:21:12 -0500

    Clases de dom, estamos en la hora 2:30 del video

--- toppings-de-pizza/app.js
+++ toppings-de-pizza/app.js
@@ -15,3 +15,60 @@
 
 // const toppingsNaranja = document.querySelectorAll('.toppings.fondo-naranja');
 // console.log(toppingsNaranja)
+
+//Entrar al texto
+// const titulo = document.getElementById('titulo')
+
+// titulo.innerText = 'Mis Toppings Favoritos';
+// console.log('textContent')
+// console.log(listaDeToppings.textContent)
+
+// console.log('innterText')
+// console.log(listaDeToppings.innerText)
+
+// console.log('innerHtml')
+// console.log(listaDeToppings.innerHTML)
+
+// const enlace = document.getElementsByTagName('a');
+
+// console.log(enlace[0].setAttribute('href', 'https://www.freecodecamp.org'))
+
+// const primerTopping =document.querySelector('.topping');
+
+// // primerTopping.classList.add('texto-verde')
+// // primerTopping.classList.remove('topping')
+// // console.log(primerTopping.classList);
+
+// const listaDeToppings = document.getElementById('lista-toppings');
+// // const toppingNuevo = document.createElement('li');
+// // toppingNuevo.classList.add('topping', 'fondo-marron');
+
+// // toppingNuevo.innerText = 'Queso Extra';
+
+// // listaDeToppings.appendChild(toppingNuevo);
+
+// // toppingNuevo.remove();
+
+// // primer hijo por orden
+// // console.log(listaDeToppings.children[0])
+// // primer hijo
+// // console.log(listaDeToppings.firstElementChild)
+// // ultimo hijo
+// // console.log(listaDeToppings.lastElementChild)
+// // nodos hermanos
+// // anterior hermano
+// console.log(listaDeToppings.previousElementSibling)
+// console.log(listaDeToppings.previousSibling)
+// // siguiente hermano
+// console.log(listaDeToppings.nextElementSibling)
+// console.log(listaDeToppings.nextSibling)
+
+
+
+
+
+
+
+
+
+
